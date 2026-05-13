7966f8ace1a0f27de5c02f7ce570b2d9b7549126
Colin Pireyre <38034361+cpireyre@users.noreply.github.com>
2026-05-13 09:36:57 +0300

    Add: set-zeroes-in-matrix - submission-36

---
+++ Data Structures & Algorithms/set-zeroes-in-matrix/submission-36.py
@@ -0,0 +1,15 @@
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        R,C = len(matrix),len(matrix[0])
+        shouldClearTop = False
+        for r,c in product(range(R),range(C)):
+            if matrix[r][c] == 0:
+                if r == 0: shouldClearTop = True
+                else: matrix[r][0] = 0
+                matrix[0][c] = 0
+        for r,c in product(range(1,R),range(1,C)):
+            if matrix[r][0]*matrix[0][c]==0: matrix[r][c] = 0
+        if matrix[0][0] == 0:
+            for r in range(R): matrix[r][0] = 0
+        if shouldClearTop:
+            for c in range(C): matrix[0][c] = 0
