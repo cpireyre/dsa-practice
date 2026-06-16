1cd1c3d96dd497c2eb7b8caf623c1fcac3f778cb
Colin Pireyre <38034361+cpireyre@users.noreply.github.com>
2026-06-16 11:03:55 +0300

    Add: valid-sudoku - submission-28

---
+++ Data Structures & Algorithms/valid-sudoku/submission-28.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        rows = [0] * 9
+        cols = [0] * 9
+        squa = [0] * 9
+        for r,c in product(range(9),range(9)):
+            if board[r][c] == ".": continue
+            mask = 1 << (int(board[r][c]) - 1)
+            if rows[r] & mask: return False
+            if cols[c] & mask: return False
+            if squa[3*(r//3) + c//3] & mask: return False
+            rows[r] |= mask
+            cols[c] |= mask
+            squa[3*(r//3) + c//3] |= mask
+        return True
