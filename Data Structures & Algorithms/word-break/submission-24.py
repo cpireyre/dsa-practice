d24e4aa5602322273cac0cbd2c856e34eb801d4e
Colin Pireyre <38034361+cpireyre@users.noreply.github.com>
2026-06-18 10:12:05 +0300

    Add: word-break - submission-24

---
+++ Data Structures & Algorithms/word-break/submission-24.py
@@ -0,0 +1,9 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        dp = [False] * len(s)  + [True]
+        for i in reversed(range(len(s)+1)):
+            for w in wordDict:
+                L = len(w)
+                if s[i:i+L] == w: dp[i] = dp[i+L]
+                if dp[i]: break
+        return dp[0]
