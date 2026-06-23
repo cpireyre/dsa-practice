507f6414d3dd7482f2d17e24e54b813a25c25271
Colin Pireyre <38034361+cpireyre@users.noreply.github.com>
2026-06-23 12:50:25 +0300

    Add: task-scheduling - submission-0

---
+++ Data Structures & Algorithms/task-scheduling/submission-0.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int leastInterval(vector<char>& tasks, int n) {
+        vector<int> count(26,0);
+        for (char task : tasks) count[task - 'A'] += 1;
+        int maxf = *max_element(count.begin(), count.end());
+        int maxCount = 0;
+        for (int i : count) maxCount += i == maxf;
+        int time = (maxf - 1) * (n + 1) + maxCount;
+        return max((int)tasks.size(), time);
+    }
+};
