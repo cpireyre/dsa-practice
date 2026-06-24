d8e969a71a0acb9979152b57ea41c3e623b44fad
Colin Pireyre <38034361+cpireyre@users.noreply.github.com>
2026-06-24 15:30:27 +0200

    Add: decode-ways - submission-39

---
+++ Data Structures & Algorithms/decode-ways/submission-39.py
@@ -0,0 +1,8 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        dp = [0] * len(s) + [1]
+        for i in reversed(range(len(s))):
+            dp[i] += dp[i+1] * (s[i] != '0')
+            if i+1 == len(s): continue
+            dp[i] += dp[i+2] * (s[i]=='1' or (s[i]=='2' and s[i+1] in "0123456"))
+        return dp[0]
