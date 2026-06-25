fbc76177f51999262d409d3521a3be0bb7c781ff
Colin Pireyre <38034361+cpireyre@users.noreply.github.com>
2026-06-25 19:04:21 +0200

    Add: n-queens - submission-7

---
+++ Data Structures & Algorithms/n-queens/submission-7.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<string> board;
+    vector<bool> col,posDiag,negDiag;
+    vector<vector<string>> res;
+
+    vector<vector<string>> solveNQueens(int n) {
+        board.resize(n,string(n,'.'));
+        col.resize(n,false);
+        posDiag.resize(2*n,false);
+        negDiag.resize(2*n,false);
+        f(0,n);
+        return res;
+    }
+
+    void f(int r, int n) {
+        if (r == n) { res.push_back(board); return; }
+        for (int c = 0; c < n; c++) {
+            if (col[c] || posDiag[r+c] || negDiag[r-c+n]) continue;
+            col[c] = posDiag[r+c] = negDiag[r-c+n] = true;
+            board[r][c] = 'Q';
+            f(r+1,n);
+            board[r][c] = '.';
+            col[c] = posDiag[r+c] = negDiag[r-c+n] = false;
+        }
+    }
+};
