de09f70ef9747bdb2fa0e9e12f70240b37cef856
Colin Pireyre <38034361+cpireyre@users.noreply.github.com>
2026-07-09 16:27:39 +0300

    Add: permutation-string - submission-50

---
+++ Data Structures & Algorithms/permutation-string/submission-50.py
@@ -0,0 +1,18 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s1) > len(s2): return False
+        W,C = [0]*26,[0]*26
+        al = lambda c: ord(c) - ord('a')
+        for i in range(len(s1)):
+            C[al(s1[i])] += 1
+            W[al(s2[i])] += 1
+        match = lambda c: C[c] == W[c]
+        matches = sum(match(i) for i in range(26))
+        l = 0
+        for r,c in enumerate(s2[len(s1):]):
+            if matches == 26: return True
+            p,q = al(c),al(s2[l])
+            matches -= match(p) + match(q)
+            W[p] += 1; W[q] -= 1; l += 1
+            matches += match(p) + match(q)
+        return matches == 26
